77c12a37c5f19ea70c183a063a5179acfbb5332f
Kavachi Ukegbu <michelle.ukegbu@gmail.com>
2020-03-30 19:33:40 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,21 +1,20 @@
-function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
+var lyric = "maybe";
+function callMe(){
+console.log("I just met you...");
+console.log("and this is crazy..");
+console.log("but here's my number..");
+console.log("so call me");
+return lyric;
 }
+callMe();
 
 
+thisIsCrazy();
 function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
-  }
+var thisIsCrazy = function ()// fix the code in here:
+  console.log("hey!!!");
 }
+
 
 function sayMyName() {
   // fix the code in here:
